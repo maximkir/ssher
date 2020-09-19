57e4a8e1308aea4c47042129bceab8f24441dcc7
Maxim Kirilov <kir.maxim@gmail.com>
2020-09-19 21:49:18 +0300

    WIP

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='ssher',
     version='0.1',
+    author='Maxim Kirilov',
+    author_email='kir.maxim@gmail.com',
     py_modules=['ssher'],
     install_requires=[
         'Click==7.1.2',
